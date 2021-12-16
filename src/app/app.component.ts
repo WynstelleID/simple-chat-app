058ef9e8e7a5b0bfa544b29947fde5d1ae771ef8
dandi setiyawan <setiyawandandi@gmail.com>
2021-12-16 11:02:13 +0700

    Add existing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'simple-chat-app';
+  newMessage: string;
+  messageList: string[] = [];
+
+  constructor(private chatService: ChatService) {
+
+  }
+
+  ngOnInit() {
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
 }
